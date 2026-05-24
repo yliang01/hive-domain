6be5421fb4da63277eaebeb42a85544382fa4ad8
YLiang <yliang01@test.com>
2026-05-25 02:34:32 +0800

    commit

--- src/main/java/cc/cc3c/hive/domain/repository/FileGroupRecordRepository.java
+++ src/main/java/cc/cc3c/hive/domain/repository/FileGroupRecordRepository.java
@@ -2,8 +2,10 @@ package cc.cc3c.hive.domain.repository;
 
 import cc.cc3c.hive.domain.entity.FileGroupRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.List;
@@ -15,6 +17,11 @@ public interface FileGroupRecordRepository extends JpaRepository<FileGroupRecord
     Optional<FileGroupRecord> findByHiveRecord_IdAndGroup_Category_Code(Integer hiveRecordId, String categoryCode);
 
     long countByGroup_Id(Long groupId);
+
+    @Transactional
+    @Modifying
+    @Query("delete from FileGroupRecord gr where gr.hiveRecord.id = :hiveRecordId")
+    void deleteByHiveRecord_Id(@Param("hiveRecordId") Integer hiveRecordId);
 
     @Query("select gr.hiveRecord.id from FileGroupRecord gr where gr.group.category.code = :categoryCode")
     List<Integer> findHiveRecordIdsByCategoryCode(@Param("categoryCode") String categoryCode);
